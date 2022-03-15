99c6e22b944e34d8860bab62875ac21c8d0b49a1
Noah Chernet <noahchernet@yahoo.com>
2022-03-15 20:13:33 +0300

    Error fixed for 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,6 +11,6 @@ exec {'configure_nginx':
   new_url="\troot \/var\/www\/html;\n\trewrite ^\/redirect_me$ https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;";
   sudo sed -i "s/\troot \/var\/www\/html;/$new_url/" /etc/nginx/sites-available/default;
   sudo service nginx reload;
-  '
+  ',
   provider => 'shell'
 }
