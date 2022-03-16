53a4b6446d943e93efc483d510beafcb816352f2
Noah Chernet <noahchernet@yahoo.com>
2022-03-16 16:04:52 +0300

    Refactored to run existing bash scripts

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,9 @@
 
 exec {'configure_nginx':
   command  => '
-  sudo apt install nginx -y
-  sudo rm /var/www/html/index.nginx-debian.html;
-  sudo echo "Hello World!" | sudo tee -a /var/www/html/index.nginx-debian.html;
-  sudo sed -i "s/\tlisten [0-9].* default_server;*/\tlisten 80 default_server;/" /etc/nginx/sites-available/default;
-  sudo sed -i "s/\tlisten \[::\]:[0-9].* default_server;*/\tlisten [::]:80 default_server;/" /etc/nginx/sites-available/default;
-  new_url="\troot \/var\/www\/html;\n\trewrite ^\/redirect_me$ https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;";
-  sudo sed -i "s/\troot \/var\/www\/html;/$new_url/" /etc/nginx/sites-available/default;
-  sudo service nginx reload;
+  ./1-install_nginx_web_server 
+  ./3-redirection
+  ./4-not_found_page_404
   ',
   provider => 'shell'
 }
