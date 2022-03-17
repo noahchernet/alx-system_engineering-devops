392cbf08930f4c06aeca9cfd4c0fffcf05485e6d
Noah Chernet <noahchernet@yahoo.com>
2022-03-17 13:27:13 +0300

    Reusing existing code for Puppet script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,8 @@
 
 exec {'configure_nginx':
   command  => '
-  sudo apt install nginx -y
-  sudo rm /var/www/html/index.nginx-debian.html;
-  sudo echo "Hello World!" | sudo tee -a /var/www/html/index.nginx-debian.html;
-  sudo sed -i "s/\tlisten [0-9].* default_server;*/\tlisten 80 default_server;/" /etc/nginx/sites-available/default;
-  sudo sed -i "s/\tlisten \[::\]:[0-9].* default_server;*/\tlisten [::]:80 default_server;/" /etc/nginx/sites-available/default;
-  new_url="\troot \/var\/www\/html;\n\trewrite ^\/redirect_me$ https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;";
-  sudo sed -i "s/\troot \/var\/www\/html;/$new_url/" /etc/nginx/sites-available/default;
-  sudo service nginx restart;
+  ./1-install_nginx_web_server
+  ./3-redirection
   ',
   provider => 'shell'
 }
