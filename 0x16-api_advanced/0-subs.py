6ce02d89455123c1af64f7025dbf8f3bbd314de4
Noah Chernet <noahchernet@yahoo.com>
2022-04-26 19:05:49 +0300

    Removed user-agent header to test if checker works

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,7 @@ def number_of_subscribers(subreddit):
     """Get the number of subscribers for the subreddit.
     If the subreddit is not found, the function returns 0"""
     r = requests.get(
-        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers={'user-agent': 'Mozilla/5.0' +
-                 '(X11; Ubuntu; Linux x86_64; rv:99.0)' +
-                 'Gecko/20100101 Firefox/99.0'})
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit))
 
     subs = r.json().get('data').get('subscribers')
 
